143c5ebdeb401c5758d0f4ffb217299fbd52d6ef
Jarod <jbnaidoo907@gmail.com>
2023-02-17 09:07:09 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+*main - Entry point
+*return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\*Programming is like building a multilingual puzzel");
+		return (0);
+}
